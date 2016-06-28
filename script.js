1b0458b33229058e40117ce4a4af62607ea2e319
DPGoertzen <dpgoertzen@gmail.com>
2016-06-28 15:02:27 -0500

    our initial vars declared

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function randomNumber(min, max) {
 	return Math.floor(Math.random() * (1 + max - min) + min);
 }
+
+function Fruit(type, price){
+	this.type = type;
+	this.price = price;
+}
+
+var fruitArray = [];
+
+$(document).onReady(function(){
+
+});
